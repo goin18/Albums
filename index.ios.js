8dcf722d662fdcbc3c362a9e2a111300ae2236d9
Marko Budal <markobudal@gmail.com>
2016-12-20 16:01:17 +0100

    First Commit - Album App

---
+++ index.ios.js
@@ -0,0 +1,23 @@
+//Index.ios.js - place code in here for IOS
+
+
+//Import a library to help create a Component
+import React from 'react';
+// import ReactNative from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
+import Header from './src/components/header.js';
+import AlbumList from './src/components/AlbumList.js'
+
+//Create a Component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums!'}/>
+      <AlbumList />
+    </View>
+
+  );
+};
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail.js'
+
+//https://rallycoding.herokuapp.com/api/music_albums
+class AlbumList extends Component {
+  state = { albums: [] };
+
+
+  componentWillMount() {
+    console.log('componentWillMount in Albumlist');
+    // debugger;
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data })
+    );
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    //  <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
+  render() {
+    console.log(this.state.albums);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
